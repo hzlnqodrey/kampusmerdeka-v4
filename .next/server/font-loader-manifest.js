f94ef3eb9f1b026f856029ca8c76b4270ca36370
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2023-01-08 21:55:19 +0700

    remove gitignore files

--- .next/server/font-loader-manifest.js
+++
@@ -1,8 +0,0 @@
-self.__FONT_LOADER_MANIFEST={
-  "pages": {},
-  "app": {
-    "C:\\.qodri-and-his-frontend-adventure\\NextJS-Projects\\kampusmerdeka-v4\\node_modules\\@next\\font\\google\\target.css?{\"path\":\"app\\\\page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}": [
-      "static/media/2aaf0723e720e8b9.p.woff2"
-    ]
-  }
-}
