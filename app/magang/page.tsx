32a10b50712a932c5745a558d088b9c71f323630
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2023-01-10 17:26:11 +0700

    create magang and studpen page

---
+++ app/magang/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "magang.module.css"
+
+export default function Magang() {
+    return (
+        <>
+            <h1>Magang</h1>
+        </>
+    )
+}
---
+++ app/studi-independen/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "magang.module.css"
+
+export default function StudiIndependen() {
+    return (
+        <>
+            <h1>Studi Independen</h1>
+        </>
+    )
+}
